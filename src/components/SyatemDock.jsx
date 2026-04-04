47dfbf37d890ded27b2392e60aa55552487fc461
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-04 14:53:34 -0400

    Delete SyatemDock.jsx

--- src/components/SyatemDock.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function SystemDock() {
-  return (
-    <div style={{ padding: "20px", color: "white" }}>
-      System Dock Loaded
-    </div>
-  );
-}
