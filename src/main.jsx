c1cf517a2cbe33a57baba3af436d53bd4c866ee6
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-04 14:29:43 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { inject } from "@vercel/analytics"
+inject()
 
-// This MUST match the id="root" in your index.html
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import "./index.css"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
