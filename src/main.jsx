456ff73b1e6d8d7db72fc097f82f4be23a498620
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-06 15:23:24 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
-import ContactPage from "./pages/Contact";
-
-const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/services", element: <Services /> },
-  { path: "/contact", element: <ContactPage /> }
-]);
+import Contact from "./pages/Contact";
+import MemberPortal from "./pages/MemberPortal";
+import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="services" element={<Services />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="portal" element={<MemberPortal />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
