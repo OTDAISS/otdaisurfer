c1fc42a4e60713159d79e7ba6803a73b36e4af3c
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-08 12:17:21 -0400

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,27 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App";
+import Home from "./pages/Home";
+import MembershipGate from "./components/MembershipGate";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <MembershipGate>
+        <App />
+      </MembershipGate>
+    ),
+    children: [
+      { index: true, element: <Home /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
