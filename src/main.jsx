836a8396367cd185ce476996a9218f9416cfbb02
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-08 18:42:54 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import router from "./router"; // Make sure you have router.js or router.jsx in src/
-
-import "./index.css"; // Only if you have Tailwind or global styles
+import router from "./router.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
