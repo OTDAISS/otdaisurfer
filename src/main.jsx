42bb2d7efef38ba8cdb106baacd59ce62c0efbd1
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-08 00:27:40 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
+
+// Pages
 import Home from "./pages/Home";
 import Services from "./pages/Services";
+import Realm from "./pages/Realm";
 import Contact from "./pages/Contact";
-import AboutPage from "./pages/AboutPage";
+import Membership from "./pages/Membership";
 
-import "./styles/global.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "services", element: <Services /> },
-      { path: "contact", element: <Contact /> },
-      { path: "about", element: <AboutPage /> },
-    ],
-  },
-]);
+// Global Styles
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+
+        {/* App Shell */}
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="services" element={<Services />} />
+          <Route path="realm" element={<Realm />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="membership" element={<Membership />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
